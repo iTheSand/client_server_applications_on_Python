5ad98179ee593b1455c0ce8070c5de2874886fbe
iTheSand <thesand2009@yandex.ru>
2021-07-07 18:25:10 +0300

    lesson_6_hw

---
+++ server.py
@@ -0,0 +1,73 @@
+from socket import *
+import sys
+import json
+import logging
+import logs.server_log_config
+from logs.server_decorator import trace
+
+from utils import configs, send_message, get_message
+
+CONFIGS = configs()
+SERVER_LOGGER = logging.getLogger('server')
+
+
+@trace
+def handle_message(message):
+    if CONFIGS.get('ACTION') in message \
+            and message[CONFIGS.get('ACTION')] == CONFIGS.get('PRESENCE') \
+            and CONFIGS.get('TIME') in message \
+            and CONFIGS.get('USER') in message \
+            and message[CONFIGS.get('USER')][CONFIGS.get('ACCOUNT_NAME')] == 'Guest':
+        SERVER_LOGGER.info('Клиенту сформировано успешное сообщение')
+        return {CONFIGS.get('RESPONSE'): 200}
+    SERVER_LOGGER.critical('Клиенту сформировано сообщение об ошибке')
+    return {
+        CONFIGS.get('RESPONSE'): 400,
+        CONFIGS.get('ERROR'): 'Bad Request'
+    }
+
+
+@trace
+def main():
+    try:
+        if '-p' in sys.argv:
+            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            listen_port = CONFIGS.get('DEFAULT_PORT')
+        if not 65535 >= listen_port >= 1024:
+            raise ValueError
+    except IndexError:
+        SERVER_LOGGER.error('После -\'p\' необходимо указать порт')
+        sys.exit(1)
+    except ValueError:
+        SERVER_LOGGER.error('Порт должен быть указан в пределах от 1024 до 65535')
+        sys.exit(1)
+
+    try:
+        if '-a' in sys.argv:
+            listen_address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            listen_address = ''
+    except IndexError:
+        SERVER_LOGGER.error('После -\'a\' необходимо указать адрес')
+        sys.exit(1)
+
+    transport = socket(AF_INET, SOCK_STREAM)
+    transport.bind((listen_address, listen_port))
+    transport.listen(CONFIGS.get('MAX_CONNECTIONS'))
+
+    while True:
+        client, client_address = transport.accept()
+        try:
+            message = get_message(client, CONFIGS)
+            response = handle_message(message)
+            send_message(client, response, CONFIGS)
+            SERVER_LOGGER.info(f'Получено сервисное сообщение о присутствии')
+            client.close()
+        except (ValueError, json.JSONDecodeError):
+            SERVER_LOGGER.error('Принято некорретное сообщение от клиента')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_server.py
@@ -0,0 +1,52 @@
+import unittest
+import time
+import sys
+
+sys.path.append('../')
+
+from server import handle_message
+from utils import configs
+
+
+class TestServer(unittest.TestCase):
+    CONFIGS = configs()
+
+    success_message = {CONFIGS['RESPONSE']: 200}
+
+    error_message = {
+        CONFIGS['RESPONSE']: 400,
+        CONFIGS['ERROR']: 'Bad Request'
+    }
+
+    def test_without_action(self):
+        self.assertEqual(
+            handle_message({
+                self.CONFIGS['TIME']: 1.1,
+                self.CONFIGS['USER']: {
+                    self.CONFIGS['ACCOUNT_NAME']: 'Guest'
+                }
+            }), self.error_message)
+
+    def test_no_success_data(self):
+        self.assertEqual(
+            handle_message({
+                self.CONFIGS['ACTION']: self.CONFIGS['RESPONSE'],
+                self.CONFIGS.get('TIME'): time.time(),
+                self.CONFIGS.get('USER'): {
+                    self.CONFIGS.get('ACCOUNT_NAME'): 'Guest',
+                }
+            }), self.error_message)
+
+    def test_success_data(self):
+        self.assertEqual(
+            handle_message({
+                self.CONFIGS['ACTION']: self.CONFIGS['PRESENCE'],
+                self.CONFIGS.get('TIME'): time.time(),
+                self.CONFIGS.get('USER'): {
+                    self.CONFIGS.get('ACCOUNT_NAME'): 'Guest',
+                }
+            }), self.success_message)
+
+
+if __name__ == '__main__':
+    unittest.main()
